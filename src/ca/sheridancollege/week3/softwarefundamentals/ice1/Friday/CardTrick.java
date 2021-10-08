074f480ae4a84a0bb5cc23004468895b33524590
JohnM1313 <millajoh@shernet.sheridancollege.ca>
2021-10-08 17:10:43 -0400

    First time Committing

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -5,10 +5,11 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
  *
  * @author Ronak
  */
+//John Millar 991608664
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card1 = new Card();
     }
     
 }
